350146640e23d44bb00ec4255509275c36245413
takumu <takumu155695@gmail.com>
2022-04-14 22:17:41 +0900

    Add: コンポーネントディレクトリ

---
+++ src/components/ColoredMessage.jsx
@@ -0,0 +1,13 @@
+export const ColoredMessage = (props) => {
+
+    const {color, children} = props;
+
+    const contentStyle = {
+        color: color,
+        fontSize: "20px"
+    };
+
+    return(
+        <p style={contentStyle}>{children}</p>
+    )
+}
